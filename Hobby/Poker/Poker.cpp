faf270dcc918d1bb56e6624eb5c99331a184903a
MrDeun <pmichalak16@gmail.com>
2023-03-09 17:36:40 +0100

    kontruktor z pliku

---
+++ Hobby/Poker/Poker.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include<Windows.h>
+struct Karta
+{
+    int Wartosc;
+    int Kolor;
+    bool Wyciagnieta=false;
+    std::string RozszyfrujKolor(int Kolor)
+    {
+        switch (Kolor)
+        {
+        case 0:return "Serce";
+        case 1:return "Wino";
+        case 2:return "Zoladz";
+        case 3:return "Diament";
+        }
+    }
+
+    std::string RozszyfrujWartosc(int Kolor)
+    {
+        switch (Kolor)
+        {
+        case 0:return "9";
+        case 1:return "10";
+        case 2:return "J";
+        case 3:return "Q";
+        case 4:return "K";
+        case 5:return "A";
+        }
+    }
+
+};
+
+void Generate(Karta *Deck)
+{
+    for (int Licznik = 0; Licznik < 4; Licznik++)
+    {
+        for (int LicznikDrugi = 0; LicznikDrugi < 6; LicznikDrugi++)
+            Deck[LicznikDrugi + (6 * Licznik)].Kolor = Licznik % 4;
+    }
+
+    for (int Licznik = 0; Licznik < 24; Licznik++)
+        Deck[Licznik].Wartosc = Licznik % 6;
+    return;
+
+
+}
+void Show(Karta* Deck, int Rozmiar)
+{
+    //system("CLS");
+    std::cout << std::endl;
+    for (int Licznik = 0; Licznik < Rozmiar; Licznik++)
+    {
+        std::cout << Deck[Licznik].RozszyfrujKolor(Deck[Licznik].Kolor);
+        std::cout << Deck[Licznik].RozszyfrujWartosc(Deck[Licznik].Wartosc);
+        std::cout << "    ";
+
+        if (Licznik % 6 == 5)
+            std::cout << std::endl;
+    }
+    return;
+}
+
+void Randomize(Karta* Deck, int DeckSize)
+{
+    int currentIndex=DeckSize,randomIndex;
+
+    while(currentIndex!=0)
+    {
+        randomIndex = rand()%currentIndex;
+        currentIndex--;
+        std::swap(Deck[randomIndex],Deck[currentIndex]);
+    }
+
+    return;
+}
+
+void GiveTheCard()
+{
+    
+}
+
+int main()
+{
+    const int Rozmiar = 24;
+    Karta *Deck = new Karta[Rozmiar];
+    Generate(Deck);
+    Show(Deck, Rozmiar);
+    Randomize(Deck, Rozmiar);
+    Show(Deck, Rozmiar);
+    system("PAUSE");
+    return 0;
+}
--- JezykiProgramowania/ObjectCPP/WektorKlasy/wektorclass.cpp
+++ JezykiProgramowania/ObjectCPP/WektorKlasy/wektorclass.cpp
@@ -1,56 +1,64 @@
-#include<fstream>
-#include<iostream>
-
+#include <fstream>
+#include <iostream>
 
 class Vector
 {
-    private:
-        int VectorCount;
-        float *VectorValue;
-    public:
-        Vector(int VectorCount_in)
+private:
+    int VectorCount;
+    float *VectorValue;
+
+public:
+    Vector(int VectorCount_in)
+    {
+        VectorCount = VectorCount_in;
+        VectorValue = new float[VectorCount_in];
+    }
+    Vector(const char *File_Name)
+    {
+        std::ifstream File(File_Name);
+
+        File >> VectorCount;
+        VectorValue = new float[VectorCount];
+        int LoopCounter;
+        for(;LoopCounter<VectorCount;LoopCounter++)
+            File>>VectorValue[LoopCounter];
+    }
+    ~Vector()
+    {
+        VectorCount = 0;
+        delete[] VectorValue;
+    }
+    void Show();
+    void WriteToFile(const char *File_Name);
+    void ReadFromFile(const char *File_Name);
+    bool Set(int VectorCount_in, float VectorValue_in)
+    {
+        if (VectorCount_in >= 0 && VectorCount_in < VectorCount)
         {
-            VectorCount = VectorCount_in;
-            VectorValue = new float[VectorCount_in];
+            VectorValue[VectorCount_in] = VectorValue_in;
+            return true;
         }
-        ~Vector()
-        {
-            VectorCount = 0;
-            delete[] VectorValue;
-        }
-        void Show();
-        void WriteToFile(const char *File_Name);
-        void ReadFromFile(const char *File_Name);
-        bool Set(int VectorCount_in, float VectorValue_in)
-        {
-            if(VectorCount_in>=0&&VectorCount_in<VectorCount)
-            {
-                VectorValue[VectorCount_in]=VectorValue_in;
-                return true;
-            }
-                return false;
-        }
-        float Get(int VectorCount_in)
-        {
-                return VectorValue[VectorCount_in];
-        }
-        
+        return false;
+    }
+    float Get(int VectorCount_in)
+    {
+        return VectorValue[VectorCount_in];
+    }
 };
 
 void Vector::Show()
 {
-        std::cout << "(";
+    std::cout << "(";
     for (int i = 0; i < VectorCount; i++)
     {
         std::cout << VectorValue[i];
         if (i != VectorCount - 1)
             std::cout << ", ";
-
     }
     std::cout << ")\n";
 }
 
-void Vector::WriteToFile(const char* File_Name)
+void Vector::WriteToFile(const char *File_Name)
 {
     std::ofstream File(File_Name);
     File << VectorCount << std::endl;
@@ -60,14 +68,14 @@ void Vector::WriteToFile(const char* File_Name)
     File.close();
 }
 
-void Vector::ReadFromFile(const char* File_Name)
+void Vector::ReadFromFile(const char *File_Name)
 {
     std::ifstream File(File_Name);
-    if(VectorValue)
+    if (VectorValue)
         delete[] VectorValue;
-    File>>VectorCount;
-    VectorValue=new float[VectorCount];
-        for (int i = 0; i < VectorCount; i++)
+    File >> VectorCount;
+    VectorValue = new float[VectorCount];
+    for (int i = 0; i < VectorCount; i++)
     {
         File >> VectorValue[i];
     }
@@ -75,5 +83,28 @@ void Vector::ReadFromFile(const char* File_Name)
 }
 int main()
 {
-    Vector Vector1(5);
+    // Vector Vector1(5);
+    // for (int LoopCounter = 0; LoopCounter < 5; LoopCounter++)
+    // {
+    //     Vector1.Set(LoopCounter, LoopCounter * 2.5);
+    // }
+    // Vector1.Show();
+    // Vector *Vector2 = new Vector(3);
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     float Wartosc;
+    //     std::cin >> Wartosc;
+    //     Vector2->Set(i, Wartosc);
+    // }
+    // Vector2->Show();
+    // Vector1.WriteToFile("w1.txt");
+    // Vector2->ReadFromFile("w5.txt");
+    // Vector2->Show();
+
+    Vector v("w1.txt");
+    v.Show();
+
+    // std::cout << Vector1.Get(-2);
+    // delete Vector2;
+    return 0;
 }
