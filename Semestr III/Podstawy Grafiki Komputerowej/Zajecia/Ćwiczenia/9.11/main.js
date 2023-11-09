755924af366f6fb409ab930cae1f7380e89e5869
MrDeun <59258041+MrDeun@users.noreply.github.com>
2023-11-09 13:40:51 +0100

    Graphics - 9.11.2023

--- Semestr III/Podstawy Grafiki Komputerowej/Zajecia/Ćwiczenia/9.11/main.js
+++ Semestr III/Podstawy Grafiki Komputerowej/Zajecia/Ćwiczenia/9.11/main.js
@@ -3,12 +3,19 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
+
 const scene = new THREE.Scene();
+const ambiencelight = new THREE.AmbientLight(0x404040,2.0);
+const testLight = new THREE.SpotLight(0xffffff,2.0,0,Math.PI/4,0,2.0);
 const camera = new THREE.PerspectiveCamera(90,window.innerWidth/window.innerHeight,0.1,1000);
 const renderer = new THREE.WebGLRenderer();
 const controls = new OrbitControls( camera, renderer.domElement );
 controls.update();
 const loader = new GLTFLoader();
+
+
+const axesHelper = new THREE.AxesHelper(200);
+
 
 //Ustawienia Kamery
 
@@ -16,17 +23,27 @@ camera.position.z = 20;
 //camera.rotation.y = Math.PI/4;
 camera.lookAt(scene.position)
 
+
 //Ustawienia Renderera
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setClearColor( new THREE.Color( 0x5588ff ) );
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+testLight.castShadow = true;
+
+
+scene.add(ambiencelight);
+scene.add(testLight);
+scene.add(axesHelper);
+
+testLight.position.set(0,50,0)
+
 //Geometria drogii
 const verticalRoadGeometry = new THREE.PlaneGeometry(50.0,5.0,1,1)
 const horizontalRoadGeometry = new THREE.PlaneGeometry(5.0,50.0,1,1)
 //Material drogi
-const roadMaterial = new THREE.MeshBasicMaterial({color:0x777777})
+const roadMaterial = new THREE.MeshStandardMaterial({color:0x777777})
 //Meshe drogi
 const verticalRoadMesh = new THREE.Mesh(verticalRoadGeometry,roadMaterial);
 const horizontalRoadMesh = new THREE.Mesh(horizontalRoadGeometry,roadMaterial);
@@ -38,7 +55,7 @@ crossroad.add(horizontalRoadMesh);
 //Geometria trawnika
 const grassPlaneGeometry = new THREE.PlaneGeometry(50.0,50,1,1);
 //Material trawnika
-const grassPlaneMaterial = new THREE.MeshBasicMaterial({color:0x005500})
+const grassPlaneMaterial = new THREE.MeshStandardMaterial({color:0x005500})
 //Mesh trawnika
 const grassPlaneMesh = new THREE.Mesh(grassPlaneGeometry,grassPlaneMaterial);
 //Pozycja Trawnika
@@ -48,8 +65,8 @@ grassPlaneMesh.position.z = -0.01;
 const treeCrownGeometry = new THREE.SphereGeometry(3.5,8,8,0,Math.PI*2,0.0,Math.Pi)
 const treeStumpGeometry = new THREE.CylinderGeometry(1.0,1.25,4.0,8,2,true,0,Math.PI*2);
 //Material drzewa
-const treeCrownMaterial = new THREE.MeshBasicMaterial({color:0x00aa00})
-const treeStumpMaterial = new THREE.MeshBasicMaterial({color:0x916411})
+const treeCrownMaterial = new THREE.MeshStandardMaterial({color:0x00aa00,roughness:0,emissive:0x4f4f4f})
+const treeStumpMaterial = new THREE.MeshStandardMaterial({color:0x916411})
 //Meshe drzewa
 const treeCrown = new THREE.Mesh(treeCrownGeometry,treeCrownMaterial);
 const treeStump = new THREE.Mesh(treeStumpGeometry,treeStumpMaterial);
@@ -69,8 +86,8 @@ tree.position.z = 7.0
 const houseMainBodyGeometry = new THREE.BoxGeometry(5.0,3.0,3.0,1,1,1)
 const houseRoofGeometry = new THREE.ConeGeometry(4.0,2.0,4,1,false,0,Math.PI*2);
 //Material Domu
-const houseMainBodyMaterial = new THREE.MeshBasicMaterial({color:0x8e4738})
-const houseRoofMaterial = new THREE.MeshBasicMaterial({color:0x603100})
+const houseMainBodyMaterial = new THREE.MeshStandardMaterial({color:0x8e4738})
+const houseRoofMaterial = new THREE.MeshStandardMaterial({color:0x603100})
 //Meshes i pozycjonowanie Domu
 const houseMainBody = new THREE.Mesh(houseMainBodyGeometry,houseMainBodyMaterial);
 const houseRoof = new THREE.Mesh(houseRoofGeometry,houseRoofMaterial);
@@ -94,7 +111,13 @@ ground.rotation.x = -Math.PI/2;
 const meshScene = new THREE.Group();
 meshScene.add(ground);
 meshScene.add(tree);
-meshScene.add(house)
+meshScene.add(house);
+
+meshScene.castShadow = true;
+
+const helpSpotLight = new THREE.SpotLightHelper(testLight,0x00ffff);
+
+scene.add(helpSpotLight);
 scene.add(meshScene);
 
 const animate = function () {
