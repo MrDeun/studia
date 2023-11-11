3eebfcaa6117ca411f0c3492e18835c4d3d96152
MrDeun <59258041+MrDeun@users.noreply.github.com>
2023-11-11 17:33:05 +0100

    python zadanie 4 continuned

--- Semestr III/Języki Programowania III/PracaDomowa3/zadanie4.py
+++ Semestr III/Języki Programowania III/PracaDomowa3/zadanie4.py
@@ -1,2 +1,22 @@
-import matplotlib
+import matplotlib.pyplot as plot
+import numpy
+import math
+
+functionNames = ["print()","len()","input()","range()","type()","str()"]
+
+
+AxisX = numpy.array([i for i in range(80)])
+SinY = numpy.array([math.sin( (math.pi/8) * i ) for i in range(16)])
+TanY = numpy.array([math.tan( (math.pi/180) * i ) for i in range(720)])
+
+
+plot.plot(SinY)
+plot.title("Sin(x)")
+plot.show()
+plot.plot(TanY)
+limittan = plot.gca()
+limittan.set_ylim([-10,10])
+plot.plot()
+plot.title("Tan(x)")
+plot.show()
 
--- Semestr III/Podstawy Grafiki Komputerowej/BIG-BOI-PROJECT/script/prefabs/road/crossroad.js
+++ Semestr III/Podstawy Grafiki Komputerowej/BIG-BOI-PROJECT/script/prefabs/road/crossroad.js
@@ -1,35 +1,26 @@
 import * as THREE from 'three';
 
-//GEOMETRIES
-const grassPlaneGeometry = new THREE.PlaneGeometry(50.0,50.0,1,1);
+const groundGeometry = new THREE.PlaneGeometry(50.0,50.0,1,1);
+const groundMaterial = new THREE.MeshBasicMaterial({color:0x005500});
+
 const roadGeometry = new THREE.PlaneGeometry(10.0,50.0,1,1);
-//MATERIALS
-const grassPlaneMaterial = new THREE.MeshBasicMaterial({color:0x006600});
-const roadMaterial = new THREE.MeshBasicMaterial({color:0xaaaaaa});
-//MESHES
-const verticalRoad = new THREE.Mesh(roadGeometry,roadMaterial);
-const horizontalRoad = new THREE.Mesh(roadGeometry,roadMaterial);
-const grassMesh = new THREE.Mesh(grassPlaneGeometry,grassPlaneMaterial);
-//ROTATION ON MESHES
-horizontalRoad.rotation.z += Math.PI/2;
-grassMesh.position.z -= 0.1;
-//GROUPING
-const crossroadTemplate = new THREE.Group();
-crossroadTemplate.add(verticalRoad);
-crossroadTemplate.add(horizontalRoad);
-crossroadTemplate.add(grassMesh);
-//ROTATION ON GROUP
-crossroadTemplate.rotation.y -= Math.PI/2;
-module.exports = 
-{
+const roadMaterial = new THREE.MeshBasicMaterial({color:0x666666});
 
-    createCrossroad()
+export function createCrossroad()
     {
-        const crossroadTile = new THREE.Group();
-        crossroadTile.copy(crossroadTemplate);
-        return crossroadTile;
-    }
+        const groundMesh = new THREE.Mesh(groundGeometry,groundMaterial);
+        groundMesh.position.z -= 0.1;
 
-}
+        const verticalRoadMesh = new THREE.Mesh(roadGeometry,roadMaterial);
+        const horizontalRoadMesh = new THREE.Mesh(roadGeometry,roadMaterial);
+        horizontalRoadMesh.rotation.x += Math.PI/2;
+
+        const crossroadGroup = new THREE.Group();
+        crossroadGroup.add(verticalRoadMesh);
+        crossroadGroup.add(horizontalRoadMesh);
+        crossroadGroup.add(groundMesh);
+
+        return crossroadGroup;
+    }
 
 
