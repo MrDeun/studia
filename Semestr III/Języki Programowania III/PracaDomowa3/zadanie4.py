896779f12d524d00dd8852afa3e5fc63e6bd291c
MrDeun <pmichalak16@gmail.com>
2023-11-11 23:10:29 +0100

    python 3 done

--- Semestr III/Języki Programowania III/PracaDomowa3/zadanie4.py
+++ Semestr III/Języki Programowania III/PracaDomowa3/zadanie4.py
@@ -2,21 +2,27 @@ import matplotlib.pyplot as plot
 import numpy
 import math
 
-functionNames = ["print()","len()","input()","range()","type()","str()"]
 
+functionNames = ["str()","type()","range()","input()","len()","print()"]
+functionStats = [math.log( (6-i) * 5 ) for i in range (6)]
 
 AxisX = numpy.array([i for i in range(80)])
 SinY = numpy.array([math.sin( (math.pi/8) * i ) for i in range(16)])
 TanY = numpy.array([math.tan( (math.pi/180) * i ) for i in range(720)])
 
 
-plot.plot(SinY)
-plot.title("Sin(x)")
-plot.show()
-plot.plot(TanY)
-limittan = plot.gca()
-limittan.set_ylim([-10,10])
-plot.plot()
-plot.title("Tan(x)")
-plot.show()
+
+def performTask4():
+    plot.plot(SinY)
+    plot.title("Sin(x)")
+    plot.show()
+    plot.plot(TanY)
+    limittan = plot.gca()
+    limittan.set_ylim([-10,10])
+    plot.plot()
+    plot.title("Tan(x)")
+    plot.show()
+
+    plot.bar(functionNames,functionStats)
+    plot.show()
 
