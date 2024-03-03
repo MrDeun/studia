944d8f000c4e30088a316322b8c12c49c3897886
MrDeun <pmichalak16@gmail.com>
2024-03-03 16:22:21 +0100

    I have no idea what am I doing at this point

--- Semestr IV/AlgorytmyPraktyczne/z1_helix/AP_Michalak_z1_helix.cpp
+++ Semestr IV/AlgorytmyPraktyczne/z1_helix/AP_Michalak_z1_helix.cpp
@@ -1,69 +1,31 @@
 #include <algorithm>
-#include <cassert>
-#include <iostream>
+#include <iterator>
 #include <numeric>
 #include <vector>
-#include <cstdint>
-void fulfillVector(std::vector<short>& helix)
-{
-    size_t new_size{};
-    helix.clear();
-    std::cin >> new_size;
-    for (size_t i = 0; i < new_size; i++)
-    {
-        short buffer;
-        std::cin >> buffer;
-        helix.push_back(buffer);
-    }
-    return;
-}
+#include <iostream>
 
-int64_t findBiggestPath(std::vector<short>& helix_1,std::vector<short>& helix_2,std::vector<short>& common_elements)
-{
-    auto checkpoint_helix_1 = helix_1.begin();
-    auto checkpoint_helix_2 = helix_2.begin();
-    int common_iterator = 0;
-    int64_t result = 0;
-    do
-    {
-        auto next_checkpoint_helix_1 = std::find(checkpoint_helix_1,helix_1.end(),helix_1[common_iterator]);
-        auto next_checkpoint_helix_2 = std::find(checkpoint_helix_2,helix_2.end(),helix_2[common_iterator]);
-        
-        int64_t result_1 = std::accumulate(checkpoint_helix_1,next_checkpoint_helix_1,0);
-        int64_t result_2 = std::accumulate(checkpoint_helix_2,next_checkpoint_helix_2,0);
-        if (result_1>result_2)
-            result += result_1;
-        else
-            result += result_2;
 
-        checkpoint_helix_1 = next_checkpoint_helix_1;
-        checkpoint_helix_2 = next_checkpoint_helix_2;
-        assert(common_iterator > common_elements.size());
-        common_iterator++;
-        
-    } while (checkpoint_helix_1 != helix_1.end() || checkpoint_helix_2 != helix_2.end() || common_iterator > common_elements.size());
-    return result;
-}
-
-std::vector<short> commonElements(const std::vector<short>& helix_1,const std::vector<short>& helix_2)
+int calculate_best(const std::vector<short>& helix_1, const std::vector<short>& helix_2, const std::vector<short>& common_elements)
 {
-    std::vector<short> result;
-    std::set_intersection(helix_1.begin(),helix_1.end(),
-                            helix_2.begin(),helix_2.end(),
-                            std::back_inserter(result));
-    return result;
+
 }
 
 int main()
 {
-    std::vector<short> helix_1, helix_2;
-    while(true)
-    {
-        fulfillVector(helix_1);
-        if (helix_1.size() == 0) return 0;
-        fulfillVector(helix_2);
-        std::vector<short> common_elements = commonElements(helix_1,helix_2);
-        std::cout << findBiggestPath(helix_1,helix_2,common_elements);
-    }
+    std::vector<short> helix_1{3, 5, 7, 9, 20, 25, 30, 40, 55, 56, 57, 60, 62};
+    std::vector<short> helix_2{1, 4, 7, 11, 14, 25, 44, 47, 55, 57, 100};
 
+    std::vector<short> common_elements;
+    std::set_intersection(
+        helix_1.begin(),helix_1.end(),
+        helix_2.begin(),helix_2.end(),
+        std::back_inserter(common_elements)
+    );
+
+    std::reverse(common_elements.begin(),common_elements.end());
+    std::cout << calculate_best(helix_1,helix_2,common_elements);
+    
+
+
+    return 0;
 }
--- Semestr IV/AlgorytmyPraktyczne/z1_penalty/AP_Michalak_z1_penalty.cpp
+++ Semestr IV/AlgorytmyPraktyczne/z1_penalty/AP_Michalak_z1_penalty.cpp
@@ -1,39 +1,84 @@
+#include <cassert>
 #include <iostream>
-
-struct FIFA
-{
-    int group_count;
-    int teams_per_group;
-    int advance_to_knockout;
-    int bypass_to_knockout;
+#include <cstdint>
+struct Data {
+    int32_t groups;
+    int32_t teams_per_group;
+    int32_t advancees;
+    int32_t direct;
 };
 
-bool should_exit(const FIFA& fifaInstance)
-{
-    return (fifaInstance.advance_to_knockout == -1 &&
-            fifaInstance.bypass_to_knockout == -1 &&
-            fifaInstance.teams_per_group == -1 &&
-            fifaInstance.group_count == -1);
+bool is_power_of_two(long long x) {
+    return !(x == 0) && !(x & (x - 1));
 }
-void CollectData(FIFA& fifaInstance)
+
+int mylog2 (unsigned int val) {
+    if (val == 0) return UINT_MAX;
+    if (val == 1) return 0;
+    unsigned int ret = 0;
+    while (val > 1) {
+        val >>= 1;
+        ret++;
+    }
+    return ret;
+}
+
+
+void collectData(Data& our_data)
 {
-    std::cin >> fifaInstance.group_count 
-             >> fifaInstance.teams_per_group 
-             >> fifaInstance.advance_to_knockout
-             >> fifaInstance.bypass_to_knockout;
+    std::cin >> our_data.groups >> our_data.teams_per_group >> our_data.advancees >> our_data.direct;
+    return;
+}
+
+
+long long new_teams(const Data& our_data)
+{
+    long long _new_teams = 0;
+    while (!is_power_of_two((our_data.groups * our_data.advancees) + our_data.direct + _new_teams)) {
+        _new_teams++;
+    }
+
+    return _new_teams;
+}
+long long matches_in_knockout(long long power_of_two)
+{
+    if(power_of_two == 2)
+        {
+            return 1;
+        }
+    else
+        return mylog2(power_of_two) + matches_in_knockout(power_of_two/2);
+}
+
+long long total_matches(const Data& our_data, const long long& new_teams)
+{
+    assert(is_power_of_two((our_data.advancees * our_data.groups) + new_teams + our_data.direct));
+    long long _matches_in_knockout = matches_in_knockout((our_data.advancees * our_data.groups) + new_teams + our_data.direct);
+    return _matches_in_knockout;
 }
 
 int main()
 {
-    FIFA fifaInstance;
+    Data our_data;
     while(true)
     {
-        CollectData(fifaInstance);
-        if (should_exit(fifaInstance))
-        {
-            return 0;
-        }
-        
+        collectData(our_data);
+        if (
+            our_data.groups == -1 &&
+            our_data.teams_per_group == -1 &&
+            our_data.advancees == -1 &&
+            our_data.direct == -1
+        )  return 0;
+
+        long long _new_teams = new_teams(our_data);
+        long long _total_matches = total_matches(our_data, _new_teams);
+
+        std::cout << our_data.groups 
+                  << "*" << our_data.advancees 
+                  << "/" << our_data.teams_per_group 
+                  << "+" << our_data.direct 
+                  << "=" << _total_matches
+                  << "+" << _new_teams;
     }
     
 }
