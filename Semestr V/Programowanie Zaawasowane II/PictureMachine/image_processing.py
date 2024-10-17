c80edc703c0ae03ecffee9b9c59febd8c71525d8
Patryk 'MrDeun' Michalak <pmichalak16@gmail.com>
2024-10-17 15:50:04 +0200

    Getting pretty close

--- Semestr V/Programowanie Zaawasowane II/PictureMachine/image_processing.py
+++ Semestr V/Programowanie Zaawasowane II/PictureMachine/image_processing.py
@@ -1,3 +1,4 @@
+from hmac import new
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
@@ -17,14 +18,24 @@ green = img[:,:,1]
 red = img[:,:,2]
 
 new_red = red.copy()
-new_red[new_red>200] = 0
+new_green = green.copy()
+new_blue = blue.copy()
 
-show("kurczak",red)
-show("kurczak",green)
-show("kurczak",blue)
+new_red[new_red<150] = 0
+new_green[new_green<170] = 0
+new_blue[new_blue<130] = 0
 
-plt.plot(np.histogram(blue,bins=256)[0],label="blue")
-plt.plot(np.histogram(new_red,bins=256,range=(0,255))[0],label="red")
-plt.plot(np.histogram(green,bins=256)[0],label="green")
+print(red)
+# show("kurczak",new_red)
+# show("kurczak",green)
+# show("kurczak",blue)
+show("kurczak",img)
+img[:,:,2] = new_red
+img[:,:,1] = new_green
+img[:,:,0] = new_blue
+show("kurczak",img)
+plt.plot(np.histogram(blue,bins=256,range=(0,255))[0],label="blue")
+plt.plot(np.histogram(red,bins=256,range=(0,255))[0],label="red")
+plt.plot(np.histogram(green,bins=256, range=(0,255))[0],label="green")
 plt.legend()
 plt.show()
