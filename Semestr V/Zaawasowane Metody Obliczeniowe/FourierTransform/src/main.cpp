5068cc5486bbc8e7ab1283db62cb6cf224cc200c
Patryk Michalak <pmichalak16@gmail.com>
2024-11-21 15:19:09 +0100

    Models

--- Semestr V/Zaawasowane Metody Obliczeniowe/FourierTransform/src/main.cpp
+++ Semestr V/Zaawasowane Metody Obliczeniowe/FourierTransform/src/main.cpp
@@ -1,10 +1,9 @@
-#include <cmath>
 #include <cstddef>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
-
+#include <complex>
 using points = std::vector<double>;
 
 void FourierTransform(points _points) {
@@ -44,6 +43,17 @@ std::vector<points> input() {
   return vectorPoints;
 }
 
+void ComplexFourierTransform(const points& _points){
+  const size_t points_size = _points.size();
+  for (size_t n=1; n<points_size;n++) {
+    std::complex<double> c_n = 0.0 + 0.0j;
+    for (size_t i=0;i<points_size;i++) {
+      c_n += std::polar(_points[i],2*M_PI*i*n/points_size);
+    }
+  printf("real: %f\timag: %f\n",c_n.real(),c_n.imag());
+  }
+}
+
 void show_subjects(const std::vector<points>& _points) {
   for (const auto& testSubject : _points) {
     printf("Test subject: ");
@@ -57,6 +67,6 @@ void show_subjects(const std::vector<points>& _points) {
 int main(int, char **) {
   std::cout << "Hello World\n";
   std::vector<points> subject = input();
-  show_subjects(subject);
   FourierTransform(subject[0]);
+  ComplexFourierTransform(subject[0]);
 }
