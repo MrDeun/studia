51a2d4695bbaf8ee21f2b66db5c67b06d5ecb3c4
Patryk Michalak <pmichalak16@gmail.com>
2024-12-03 00:02:36 +0100

    Started regression

---
+++ Semestr V/Zaawasowane Metody Obliczeniowe/FourierTransform/src/2d_fourier.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+test_cases = int(input())
+
+size_line = input()
+x, y = size_line.split()
+x = int(x)
+y = int(y)
+print(x,y)
+mat = []
+for i in range(y):
+    line = input()
+    mat_line = []
+    split_lines = line.split()
+    for val in split_lines:
+        mat_line.append(float(val))
+    mat.append(mat_line)
+np_mat = np.array(mat)
+print(np_mat)
+
+
+plt.imshow(np_mat,interpolation='none')
+plt.show()
+transformed = abs(np.fft.fft2(np_mat))
+plt.imshow(transformed,interpolation='none')
+plt.show()
--- Semestr V/Zaawasowane Metody Obliczeniowe/FourierTransform/src/graphs.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
-points = pd.read_csv('result.csv')
-
-plt.scatter(points['real'],points['imag'])
-plt.show()
-  
